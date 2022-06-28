e29ffea52f8f0bb81de2d5bb40316a3018be1788
acse-ns1321 <88569855+acse-ns1321@users.noreply.github.com>
2022-06-28 11:54:11 +0100

    Update Image_Loader Class

--- HIRISE_api/preprocessing/Image_Loader.py
+++ HIRISE_api/preprocessing/Image_Loader.py
@@ -12,14 +12,31 @@ warnings.filterwarnings("ignore")
 
 class Load_Hirise_Images(Dataset):
     """Hirise Image dataset."""
+    def __init__(self,
+                 path_to_images,
+                 transform=None,
+                 train=True):
+        # ------------------------------------------------------------------------------
+        # path_to_images: where you put the image dataset
+        # transform:  data transform
+        # img_size: resize all images to a standard size
+        # train: return training set or test set
+        # ------------------------------------------------------------------------------
 
-    def __init__(self, image_dir, transform=None):
-        """
+        # Load all the images and their labels
+        self.dataset = datasets.ImageFolder(path_to_images, transform=transform)
+        self.len = len(self.dataset.imgs)
+        self.path_to_images = path_to_images
 
-        """
-        self.root_dir = image_dir
+        # ------------------------------------------------------------------------------
+        # Split the data into train and test data 80 : 20
+        # ------------------------------------------------------------------------------
+        # Calculate the lengths of the vectors
+        lengths = [int(np.ceil(len(self.dataset)*0.8)), int(np.floor(len(self.dataset)*0.2))]
 
-        self.transform = transform
+
+        # Extract the images and labels   
+        self.train_dataset, self.test_dataset = torch.utils.data.random_split(self.dataset, lengths)
 
     def __len__(self):
         ...
