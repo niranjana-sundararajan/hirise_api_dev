f5cc722aaf01d90a3717e6c35ba9fdc7e9c6d5eb
acse-ns1321 <88569855+acse-ns1321@users.noreply.github.com>
2022-08-29 17:42:52 +0100

    Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -5,8 +5,12 @@ sys.path.insert(0, os.path.abspath(os.sep.join((os.curdir, '..'))))
 
 project = 'HIRISE_api'
 extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.napoleon']
+              'sphinx.ext.napoleon',
+              'sphinx.ext.autosummary']
 source_suffix = '.rst'
 master_doc = 'index'
 exclude_patterns = ['_build']
 autoclass_content = "both"
+html_theme_options = {'navigation_depth': 2}
+
+autosummary_generate = True
